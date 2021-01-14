067db3db86c6f344efe9e2580c1edf6e8d429750
beans19 <felipemarqs2015@gmail.com>
2021-01-14 17:33:15 -0300

    :sparkles: collision with walls n' stuff

--- fuga.lua
+++ fuga.lua
@@ -14,7 +14,9 @@ consts = {
 }
 
 function collidesWithMap(point)
-  blockId = mget(point.x / 8, point.y / 8)
+  blockX = point.x / 8
+  blockY = point.y / 8
+  blockId = mget(blockX, blockY)
   if blockId >= 128 then
     return true
   else
@@ -22,65 +24,73 @@ function collidesWithMap(point)
   end
 end
 
+function tryToMoveTo(myDisplacementX, myDisplacementY)
+  superiorLeft = {
+    x = player.x - 8 + myDisplacementX,
+    y = player.y - 8 - 1
+  }
+  superiorRight = {
+    x = player.x + 7 + myDisplacementX,
+    y = player.y - 8 + myDisplacementY
+  }
+  inferiorRight = {
+    x = player.x + 7 + myDisplacementX,
+    y = player.y + 7 + myDisplacementY
+  }
+  inferiorLeft = {
+    x = player.x - 8 + myDisplacementX,
+    y = player.y + 7 + myDisplacementY
+  }
+  if collidesWithMap(inferiorRight) or collidesWithMap(inferiorLeft) or collidesWithMap(superiorRight) or
+    collidesWithMap(superiorRight) then
+  else
+    player.y = player.y + myDisplacementY
+    player.x = player.x + myDisplacementX
+  end
+end
+
 function update()
   -- up
   if btn(0) then
-    superiorLeft = {
-      x = player.x - 8,
-      y = player.y - 8 - 1
-    }
-    superiorRight = {
-      x = player.x + 8,
-      y = player.y - 8 - 1
-    }
-
-    if collidesWithMap(superiorLeft) or collidesWithMap(superiorRight) then
-    else
-      player.y = player.y - 1;
-    end
-
+    tryToMoveTo(0, -1)
   end
   -- down
   if btn(1) then
-    player.y = player.y + 1;
+    tryToMoveTo(0, 1)
   end
   -- left
   if btn(2) then
-    player.x = player.x - 1;
+    tryToMoveTo(-1, 0)
   end
   -- right
   if btn(3) then
-    player.x = player.x + 1;
+   tryToMoveTo(1, 0)
   end
 end
 
 function drawMap()
-  map(
-    0, -- position x in the map
-    0, -- position y in the map
-    consts.screenWidth, consts.screenHeight, 0, -- in which point x will be set
-    0 -- in which point y will be set
-  )
+  map(0, -- position x in the map
+  0, -- position y in the map
+  consts.screenWidth, consts.screenHeight, 0, -- in which point x will be set
+  0) -- in which point y will be set
 end
 
 function drawPlayer()
-  spr(
-    player.sprite, player.x - 8, player.y - 8, 0, -- background color
-    1, -- scale
-    0, -- mirror
-    0, -- rotate
-    2, -- quantity of blocks to the right
-    2  -- quantity of blocks to the left
-  ) 
+  spr(player.sprite, player.x - 8, player.y - 8, 0, -- background color
+  1, -- scale
+  0, -- mirror
+  0, -- rotate
+  2, -- quantity of blocks to the right
+  2) -- quantity of blocks to the left
 end
 
 function draw()
-  cls();
-  drawMap();
-  drawPlayer();
+  cls()
+  drawMap()
+  drawPlayer()
 end
 
 function TIC()
-  update();
-  draw();
+  update()
+  draw()
 end
